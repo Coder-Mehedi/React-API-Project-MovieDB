69f4fb3bf60eddacf9d75d43c2f6f4f17ab7d1b4
unknown <mehedi5051@gmail.com>
2020-04-23 13:44:59 +0600

    renew api key and fix all now running

--- src/components/elements/Header/Header.js
+++ src/components/elements/Header/Header.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Header.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
 
 const Header = () => (
-  <div className="rmdb-header">
-    <div className="rmdb-header-content">
-      <Link to="/">
-        <img className="rmdb-logo" src="/images/reactMovie_logo.png" alt="rmdb-logo" />
-      </Link>
-      <img className="rmdb-tmdb-logo" src="/images/tmdb_logo.png" alt="tmdb-logo" />
-    </div>
-  </div>
-)
+	<div className="rmdb-header">
+		<div className="rmdb-header-content">
+			<Link to="/">
+				<img
+					className="rmdb-logo"
+					src={`${process.env.PUBLIC_URL}/images/reactMovie_logo.png`}
+					alt="rmdb-logo"
+				/>
+			</Link>
+			<img
+				className="rmdb-tmdb-logo"
+				src={`${process.env.PUBLIC_URL}/images/tmdb_logo.png`}
+				alt="tmdb-logo"
+			/>
+		</div>
+	</div>
+);
 
 export default Header;
