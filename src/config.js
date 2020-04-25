76511cdf24194b72169707f5d89e0c9eb67ff24d
unknown <mehedi5051@gmail.com>
2020-04-26 04:15:01 +0600

    updated image base url from http to https

--- src/config.js
+++ src/config.js
@@ -11,7 +11,7 @@ const API_KEY = "bd166ff509f50be105ea5f933f5565cd";
 // An image URL looks like this example:
 // http://image.tmdb.org/t/p/w780/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg
 
-const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
 
 //Sizes: w300, w780, w1280, original
 const BACKDROP_SIZE = "w1280";
